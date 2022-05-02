d3bff4f9307b8a846e97ab4a18ff6dbe7e41eb6e
orxan666 <orxanallahyarov03@gmail.com>
2022-05-02 04:02:36 +0400

    fwfewfef

---
+++ test.py
@@ -0,0 +1,72 @@
+
+# 1ci sual
+
+# ferma = ['at', 'qoyun', 'inek', 'at', 'inek', 'qoyun', 'at', 'at', 'keci']
+# a print(ferma[-1])
+
+# b ferma.sort()
+
+# c ferma.remove("at")
+
+# c ferma.append("toyuq")
+
+# d ferma.insert(0,"keci")
+
+# f ferma2=['at', 'qoyun', 'inek', 'inek', 'qoyun']
+# print(ferma+ferma2)
+
+# g print(ferma*3)
+
+
+# h ferma.reverse()
+# print(ferma)
+
+# j ferma2=ferma.copy()
+# print(ferma2)
+
+# i inek=ferma.count("inek")
+# print(inek)
+
+# k print(ferma.clear())
+
+
+# ---------------
+
+
+# 4-cu sual
+# numbers = [10.2, -32, -5.45, 32, 7, 43, -24.06, -4, -6.33, 62]
+# print(max(numbers))
+# print(min(numbers))
+
+# --------------
+
+
+# 3-cu sual
+# daxil=input("Reqem daxil edin : ")
+# print(list( daxil[-1::-1]))
+
+# ----------------------------
+# 5-ci sual
+
+# telebeler = [31, 38, 69, 83, 83, 56, 38, 41, 96,
+#              48, 43, 60, 49, 47, 73, 60, 69, 96, 50, 74]
+# telebeler.sort()
+# print(telebeler)
+
+# # say=len(telebeler)
+# # print(say)
+
+# for i in telebeler:
+#     if i <= 51:
+#         print("kesilenler: ", i)
+#     elif i >= 51 and i < 80:
+#         print("orta bal yiganlar: ", i)
+#     elif i >= 80:
+#         print(" yuksek bal yiganlar: ", i)
+
+# -------------------------
+
+
+
+
+
